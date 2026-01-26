e72e0bf4934adcfee114f8f9f7b574765c5fd4a1
Dvormen <dvorak13@spsejecna.cz>
2026-01-26 16:34:47 +0100

    Added class using factory pattern

---
+++ src/commands/CommandFactory.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.commands
+{
+    /// <summary>
+    /// Class designed with factory pattern
+    /// </summary>
+    internal class CommandFactory
+    {
+        public static ICommand Create(string input)
+        {
+            return input.ToUpper() switch
+            {
+                "AB" => new ABCommand(),
+                "AC" => new ACCommand(),
+                "AD" => new ADCommand(),
+                "AR" => new ARCommand(),
+                "AW" => new AWCommand(),
+                "BA" => new BACommand(),
+                "BC" => new BCCommand(),
+                "BN" => new BNCommand(),
+                _ => new UnknownCommand(input)
+            };
+        }
+    }
+}
