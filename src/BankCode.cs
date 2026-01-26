7616dd03ee502d33fe07c319f9aba8fcf78949ff
Dvormen <dvorak13@spsejecna.cz>
2026-01-26 16:48:30 +0100

    Added Class for getting ip

---
+++ src/BankCode.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src
+{
+    internal class BankCode
+    {
+        public static string GetLocalIpAddress()
+        {
+            foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return null;
+        }
+    }
+}
