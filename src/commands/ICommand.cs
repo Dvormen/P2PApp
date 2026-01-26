c672ba29749b28567190233c714c91bf6642936b
Dvormen <dvorak13@spsejecna.cz>
2026-01-26 16:33:34 +0100

    Added interface for commands

---
+++ src/commands/ICommand.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.commands
+{
+    /// <summary>
+    /// An interface for commands
+    /// </summary>
+    internal interface ICommand
+    {
+        void Execute();
+    }
+}
