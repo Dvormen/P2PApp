8666d18f9f220d980bb6397b6a864e75fc8f48f7
Dvormen <dvorak13@spsejecna.cz>
2026-01-27 18:36:32 +0100

    Added new class

---
+++ src/accounts/JsonBankRepo.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.accounts
+{
+    internal class JsonBankRepo : IBankRepository
+    {
+        private readonly string _filePath = "bank_storage.json";
+
+        public BankStorage Load()
+        {
+            if (!File.Exists(_filePath))
+                return new BankStorage();
+
+            var json = File.ReadAllText(_filePath);
+
+            if (string.IsNullOrWhiteSpace(json))
+                return new BankStorage();
+
+            return JsonSerializer.Deserialize<BankStorage>(json)
+                   ?? new BankStorage();
+        }
+
+        public void Save(BankStorage data)
+        {
+            var json = JsonSerializer.Serialize(
+                data,
+                new JsonSerializerOptions { WriteIndented = true }
+            );
+
+            File.WriteAllText(_filePath, json);
+        }
+    }
+}
