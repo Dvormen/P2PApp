e58917c59af3d04d0f9d8a63618610eaaf9434dc
Dvormen <dvorak13@spsejecna.cz>
2026-01-27 18:35:55 +0100

    Added new class

---
+++ src/accounts/AccountNumberGenerator.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.accounts
+{
+    internal class AccountNumberGenerator
+    {
+        public static int GetNext(Bank bank)
+        {
+            if (!bank.Accounts.Any())
+                return 10001;
+
+            return bank.Accounts.Max(a => a.AccountNumber) + 1;
+        }
+    }
+}
