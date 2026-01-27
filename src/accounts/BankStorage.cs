92062338d77d044ca376a2bf8dba79d5e36d2532
Dvormen <dvorak13@spsejecna.cz>
2026-01-27 18:36:13 +0100

    Added new class

---
+++ src/accounts/BankStorage.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.accounts
+{
+    internal class BankStorage
+    {
+        public List<Bank> Banks { get; set; } = new();
+    }
+}
