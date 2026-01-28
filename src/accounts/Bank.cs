7d0d221388c2f74921c79eecdd5a7635821dae6f
Dvormen <dvorak13@spsejecna.cz>
2026-01-28 17:43:05 +0100

    Minor changes

--- src/accounts/Bank.cs
+++ src/accounts/Bank.cs
@@ -9,7 +9,7 @@ namespace P2PApp.src.accounts
 {
     internal class Bank
     {
-        public string BankIp { get; set; }
+        public string BankIp { get; set; } = "";
         public List<Account> Accounts { get; set; } = new();
     }
 }
