14dc9c61b349b4c6320c6e43ad3e5deada71b2fa
Dvormen <dvorak13@spsejecna.cz>
2026-01-28 17:36:07 +0100

    Moved class BankCode

---
+++ src/network/BankCode.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2PApp.src.network
+{
+    /// <summary>
+    /// Class for getting ip address
+    /// </summary>
+    internal class BankCode
+    {
+        public static string GetIp()
+        {
+            foreach (var ip in Dns.GetHostEntry(Dns.GetHostName()).AddressList)
+            {
+                if (ip.AddressFamily == AddressFamily.InterNetwork)
+                {
+                    return ip.ToString();
+                }
+            }
+
+            return null;
+        }
+    }
+}
